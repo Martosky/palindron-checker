2166ae865560b8fb842210c1839170ecd1693aaa
Martosky <imoniaromartins@yahoo.com>
2023-12-30 14:21:49 +0100

    my pallindrome app is completed and active.

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ function displayResult(){
     const inputValue = input.value;
     if(inputValue === ""){
         alert("PLease type in a word to check!");
+    }else{
+        palindromeCheck(inputValue);
     }
 
+}
+
+function palindromeCheck(str){
+    const regx = /[^A-Za-z0-9]/g;
+    const replaceReg = str.toUpperCase().replace(regx, "");
+    const compare = replaceReg.split("").reverse().join("");
+
+    if (compare === replaceReg){
+        result.innerText = `${input.value} is a palindrome.`
+    }else{
+        result.innerText = `${input.value} is not a palindrome.`
+    }
 }
